e7961deecad2cb412639c40a19c24d907835a06a
Sheikh Salman <sheikhsalman2013@gmail.com>
2022-01-08 01:18:33 +0600

    uploads folder added

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: "mysql",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
+      host: env("DATABASE_HOST", "34.126.126.170"),
       port: env.int("DATABASE_PORT", 3306),
-      database: env("DATABASE_NAME", "alienide_cms"),
+      database: env("DATABASE_NAME", "test"),
       user: env("DATABASE_USERNAME", "root"),
-      password: env("DATABASE_PASSWORD", ""),
+      password: env("DATABASE_PASSWORD", "root"),
       ssl: env.bool("DATABASE_SSL", false),
     },
   },
